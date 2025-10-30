3c80791502bdfd4ecadfee8c1b4c853e724cd092
Levishery <47386547+Levishery@users.noreply.github.com>
2025-10-30 10:11:58 +0800

    Delete growvector/DataExtraction directory
    delete personal code

--- growvector/DataExtraction/env.sh
+++
@@ -1,3 +0,0 @@
-export PATH="$PATH:/opt/conda/bin/"
-pip install cloud-volume navis connected-components-3d cilog
-#cp -r /mmdetection3d/projects/Neuron_direction /braindat/lab/liusl/Point_detection/mmdetection3d/projects/
